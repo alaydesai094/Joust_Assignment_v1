30cdcdd09020072b1894662494a8e4582b6ae304
alaydesai <alaydesai094@gmail.com>
2019-06-24 11:08:22 -0400

    Sound added to the background

--- Joust_Assignment/GameScene.swift
+++ Joust_Assignment/GameScene.swift
@@ -15,13 +15,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var enemy:SKNode!
     var player:SKNode!
     
+    // GAME STAT SPRITES
+    let livesLabel = SKLabelNode(text: "Lives: ")
+    let scoreLabel = SKLabelNode(text: "Score: ")
+    
+    
+    // GAME STATISTIC VARIABLES
+    var lives = 4
+    var score = 0
     
     override func didMove(to view: SKView)
     {
         
+        //sound
+        
+        let audio = SKAudioNode(fileNamed: "music.wav")
+        addChild(audio)
+        
         // Required for SKPhysicsContactDelegate
         self.physicsWorld.contactDelegate = self
         
+        
+        // MARK: Add a lives label
+        // ------------------------
+//        self.livesLabel.text = "Lives: \(self.lives)"
+//        self.livesLabel.fontName = "Avenir-Bold"
+//        self.livesLabel.fontColor = UIColor.magenta
+//        self.livesLabel.fontSize = 100;
+//        self.livesLabel.position = CGPoint(x:100,
+//                                           y:-200)
+//        
+        
+        // MARK: Add a score label
+        // ------------------------
+        self.scoreLabel.text = "Score: \(self.score)"
+        self.scoreLabel.fontName = "Avenir-Bold"
+        self.scoreLabel.fontColor = UIColor.magenta
+        self.scoreLabel.fontSize = 100;
+        self.scoreLabel.position = CGPoint(x:200,
+                                           y:-100)
+        
+        
+        addChild(self.livesLabel)
+        addChild(self.scoreLabel)
         
         // intialize your sprite variables
         self.player = self.childNode(withName: "player")
@@ -43,12 +79,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         
-        // Show animation for dinosaur
+        // Show animation for player
         // ----------------------------
         // 1. make an array of images for the animation
         // -- SKTexture = Object to hold images
         var dinoTextures:[SKTexture] = []
-        for i in 1...4 {
+        for i in 1...4
+        {
             let fileName = "penguin_walk0\(i)"
             print("Adding: \(fileName) to array")
             dinoTextures.append(SKTexture(imageNamed: fileName))
@@ -132,6 +169,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
         }
         
+    }
+    
+    func spawnEnemy() {
+        // lets add some enemy
+        self.enemy = self.childNode(withName:"enemy")
+        
+        //let enemy = SKSpriteNode(imageNamed: "enemy")
+        
+        // generate a random (x,y) for the enemy
+        let randX = Int(CGFloat(arc4random_uniform(UInt32(self.size.width-400))))
+        let randY = Int(CGFloat(arc4random_uniform(UInt32(self.size.height-400))))
+        
+        enemy.position = CGPoint(x:randX, y:randY)
+        
+        addChild(enemy)
+        
+        print("Where is enemy? \(randX), \(randY)")
     }
     
     var lose:Bool = false
